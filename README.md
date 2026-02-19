1a649e9ad00cb4698ac43375ebb7934343aeb57d
Josh Gibson <josh@joshuagibson.io>
2026-02-19 08:37:55 +0000

    Initial commit: My Neovim Config

---
+++ README.md
@@ -0,0 +1,19 @@
+# 💤 NeoVim Configuration for LazyVim
+
+## Pre-installation
+
+### Core CLI tools for Telescope and LazyVim extras
+
+sudo dnf install ripgrep fd-find fzf
+
+### Development tools for Treesitter (syntax highlighting)
+
+sudo dnf install gcc gcc-c++ cargo rust
+
+### Clipboard support (Required for Wayland/GNOME)
+
+sudo dnf install wl-clipboard
+
+### Language Runtimes (Required for Mason to install LSPs/Formatters)
+
+sudo dnf install nodejs npm python3-pip go luarocks
---
+++ lua/plugins/avante.lua
@@ -0,0 +1,63 @@
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    lazy = false,
+    version = false,
+    opts = {
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
+      providers = {
+        copilot = {
+          model = "gpt-4o-2024-08-06",
+        },
+      },
+      behaviour = {
+        auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+      },
+      mappings = {
+        diff = {
+          ours = "co",
+          theirs = "ct",
+          all_theirs = "ca",
+          both = "cb",
+          cursor = "cc",
+          next = "]x",
+          prev = "[x",
+        },
+      },
+    },
+    build = "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "hrsh7th/nvim-cmp",
+      "zbirenbaum/copilot.lua",
+      {
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+          },
+        },
+      },
+      {
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+}
