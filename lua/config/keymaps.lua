857a1bc8d7445eecb9acbc4339b0647c8e8c8170
Josh Gibson <jogi@trustpilot.com>
2026-02-23 13:59:29 +0000

    feat: mac paging binds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,5 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<C-j>", "<C-d>zz")
-vim.keymap.set("n", "<C-k>", "<C-u>zz")
+
+local map = vim.keymap.set
+local is_mac = vim.uv.os_uname().sysname == "Darwin"
+
+if is_mac then
+  -- macOS specific: Map the special characters Ghostty/macOS sends
+  map("n", "∆", "<C-d>zz", { desc = "Scroll Down (macOS)" })
+  map("n", "˚", "<C-u>zz", { desc = "Scroll Up (macOS)" })
+else
+  -- Linux/Other: Map the standard Alt key
+  map("n", "<M-j>", "<C-d>zz", { desc = "Scroll Down" })
+  map("n", "<M-k>", "<C-u>zz", { desc = "Scroll Up" })
+end
