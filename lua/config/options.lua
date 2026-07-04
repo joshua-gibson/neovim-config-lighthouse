1dbf20e845c4e4f3e4b8bfddee6ef03a38f283b4
Josh Gibson <josh@joshuagibson.io>
2026-07-04 10:33:30 +0100

    feat: added copilot autocomplete

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 -- vim.opt.clipboard = "unnamedplus"
+
+-- Show AI (Copilot) suggestions as inline ghost text instead of as
+-- entries in the blink.cmp popup. Accept the ghost text with <Tab>;
+-- cycle alternatives with <M-]> / <M-[>.
+vim.g.ai_cmp = false
